420556ad79bf1c100006c2fb87bda352a870dcc1
roqqq3 <rooperasa@gmail.com>
2019-06-05 14:35:28 +0300

    asd

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Niisku Mamma",
+    number: "031-4324123",
+    id: 2
+  },
+  {
+    name: "Mies Kristus",
+    number: "050-1236541",
+    id: 3
+  }
+]
+
+app.get('/info', (req, res) => {
+  let count = persons.length
+  res.send(`
+    Phonebook has info for ${count} people <br/>
+    ${new Date()}
+    `)
+})
+
+const generateId = () => {
+  return parseInt(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing'
+    })
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: 'number missing'
+    })
+  } else if (persons.find(i => i.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(i => i.id !== id)
+  res.status(204).end()
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(i => i.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
